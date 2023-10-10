17c071fad709fecc36eade1dbf584e8d037169d6
Raga <ragawilliam570@gmail.com>
2023-10-10 21:33:23 +0300

    Add files

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// using System;
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private int num { get; set; }
+
+        string name { get; set; }
+
+        private string userName { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+            
+            Console.WriteLine(program.userName);
+            // var foo = Array<int>();
+            // Console.WriteLine(foo);
+            Console.WriteLine("Hello World!");
+            // program.ame("William");
+            Console.WriteLine($"{program.num}: {program.name}");
+            Console.WriteLine(Convert.ToBoolean(program.num));
+            
+            Console.Write("Enter username: ");
+            var userName = Console.ReadLine();
+            
+            Console.WriteLine(string.Concat(userName, program.name));
+            myMethod();
+            var a = myMethod(2);
+            Console.WriteLine(a);
+        }
+
+        static void myMethod()
+        {
+            Console.WriteLine("Static method");
+        }
+
+        static int myMethod(int a)
+        {
+            return a;
+        }
+    }
+}
